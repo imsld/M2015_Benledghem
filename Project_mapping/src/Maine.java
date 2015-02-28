4d0b1808f6e110bd9e88819dcc34dd91ad98a212
Rafif <Rafif@Rafif-PC>
2015-02-28 03:05:00 +0100

    ontology mapping

---
+++ Project_mapping/src/Maine.java
@@ -0,0 +1,11 @@
+
+public class Maine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		createRDF rdf= new createRDF();
+			rdf.calcule();
+
+	}
+
+}
---
+++ Project_mapping/src/createRDF.java
@@ -0,0 +1,9 @@
+
+public class createRDF {
+	public void calcule()
+	{
+		int s=1+1;
+		System.out.println("le resultat est "+s);
+	}
+
+}
