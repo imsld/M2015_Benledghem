35c97a8e9d3c20dae0bcd335e9f1df9aa4cce349
Rafif <Rafif@Rafif-PC>
2015-03-01 19:51:17 +0100

    i try to test the jena library with this exemple but it don't work

--- Project_mapping/src/Maine.java
+++ Project_mapping/src/Maine.java
@@ -1,12 +1,24 @@
+import com.hp.hpl.jena.datatypes.xsd.XSDDatatype;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.Resource;
 
 public class Maine {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		createRDF rdf= new createRDF();
-			rdf.calcule();
-			System.out.println("force update !! ");
-			System.out.println("force update !!!!!!!!!!!!! ");
+		Model m = ModelFactory.createDefaultModel();
+		String ns = "http://Exemple.com/test " ;
+		Resource r= m.createResource(ns + "r" );
+		Property p= m.createProperty(ns + "p");
+		r.addProperty(p , "hello world" ,XSDDatatype.XSDstring);
+		m.write(System.out ,"turtle");
+		
+		
+		
+		
+		
 
 	}
 
